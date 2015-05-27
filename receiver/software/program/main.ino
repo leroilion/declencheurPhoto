5b931e39cd738a5b3b10a87f5a0e33d391d27393
Jérémy Cheynet <jerem.cheynet@gmail.com>
2015-05-27 17:37:12 +0200

    Update main file to detect with TFD frequency
    Signed-off-by: Jérémy Cheynet <jerem.cheynet@gmail.com>

--- receiver/software/program/main.ino
+++ receiver/software/program/main.ino
@@ -1,24 +1,17 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
+#include <fft.h>
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+void setup() {
+	Serial.begin(9600);
+	Serial.println(F("START"));
+
+	pinMode(13, OUTPUT);
+
+	fft_init();
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);               // wait for a second
+	if(fft_compute(FFT_K_4) > 700000000)
+		digitalWrite(13, HIGH);
+	else
+		digitalWrite(13, LOW);
 }
